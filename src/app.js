4efe7194dc00cb67a34866c11375fdcf57ae50c3
Ivan Longhi <i.longhi@ext.sirti.it>
2018-05-10 18:20:35 +0200

    modifiche grafiche

--- src/app.js
+++ src/app.js
@@ -171,3 +171,13 @@ angular
 })
 
 ;
+
+/*
+GET /:TA/ap
+POST /:TA/ap/groups # associa il gruppo a TA (eventualmente creandolo)
+PUT /:TA/ap/groups/:GROUP # modifica il gruppo (cambio descrizione o ordine), se il gruppo è usato da altri TA lo crea
+DELETE /:TA/ap/groups/:GROUP # disassocia il gruppo dal TA rendendo tutte le sue properties ungrouped, elimina il gruppo dalla tabella AP_GROUPS se non è usato da altri TA
+POST /:TA/ap/groups/:GROUP/properties # aggiunge una property al gruppo
+PUT /:TA/ap/groups/:GROUP/properties # modifica una property (posizione, readonly, hidden)
+DELETE /:TA/ap/groups/:GROUP/properties # rimuove una property dal gruppo
+*/
